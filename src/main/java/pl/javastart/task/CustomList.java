e1a17abacb15be8740c71194525261b0b4da96d2
Monika Dabrowska <monikad71@onet.pl>
2023-01-09 21:46:19 +0100

    Solution 13.4

--- src/main/java/pl/javastart/task/CustomList.java
+++ src/main/java/pl/javastart/task/CustomList.java
@@ -1,30 +1,28 @@
 package pl.javastart.task;
 
-import java.util.List;
+import java.util.Arrays;
 
 // Uzupełnij klasę. Możesz ją dowolnie modyfikować.
 // Celem jest, żeby przechodziły testy w src/test/java/[...]/CustomListTest
 public class CustomList<T> {
 
+    private static final int INITIAL_SIZE = 10;
     private int size = 0;
-    private Object[] elements;
+    private Object[] elements = {};
 
-    public Object[] getElements() {
-        return elements;
+    public CustomList() {
+        elements = new Object[INITIAL_SIZE];
     }
 
     public void setElements(T[] elements) {
         this.elements = elements;
     }
 
-    public CustomList() {
-        elements = new List[size];
-    }
-
     public void add(T a) {
         if (a == null) {
             throw new NullPointerException("Value cannot be null");
         }
+        resize();
         elements[size] = a;
         size++;
     }
@@ -33,12 +31,35 @@ public class CustomList<T> {
         if (a == null) {
             throw new NullPointerException("Value cannot be null");
         }
+        if (elements.length == size()) {
+            checkCapacity(size() * 2 + 1);
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
         elements[index] = a;
         size++;
     }
 
-    public Object size() {
-        return elements[size];
+    public void checkCapacity(int newCapacity) {
+        if (newCapacity < size) {
+            return;
+        }
+        Object[] old = elements;
+        elements = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size(); i++) {
+            elements[i] = old[i];
+        }
+    }
+
+    private void resize() {
+        if (size >= INITIAL_SIZE) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+    }
+
+    public int size() {
+        return size;
     }
 
     public T get(int index) {
@@ -56,11 +77,12 @@ public class CustomList<T> {
 
     @Override
     public String toString() {
-        String result = "";
-        for (Object element : elements) {
-            result += element.toString();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < this.size(); i++) {
+            result.append("[");
+            result.append(this.get(i));
+            result.append("]");
         }
-        return result;
+        return result.toString();
     }
-
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -10,6 +10,9 @@ public class Main {
         CustomList<String> customListOfStrings = new CustomList<>();
         CustomList<Integer> customListOfIntegers = new CustomList<>();
         customListOfStrings.add("5");
+        customListOfStrings.add("5");
+        customListOfIntegers.add(5);
+
         customListOfIntegers.add(5);
         System.out.println(customListOfStrings);
         System.out.println(customListOfIntegers);
